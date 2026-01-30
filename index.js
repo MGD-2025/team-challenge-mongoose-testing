9733baa2edf1a34c6e7ac3ef9a2ff77e730b532a
Sandra Blanco <sandra.blanco18@gmail.com>
2026-01-30 23:43:34 +0100

    Adding team challenge code

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/config.js');
+const postRouter = require ('./routes/post.js');  
+const PORT = process.env.PORT || 3002;
+
+connectDB()
+
+const app = express();
+app.use(express.json());
+app.use('/posts', postRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${process.env.PORT}`);
+});
---
+++ post.test.js
@@ -0,0 +1,1 @@
+const {addPost,getPost, getPostId, getPostTitle } = require ('./routes/post.js')
