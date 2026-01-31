a7dddc3d9de6a69465cb9a332e96b5df74babc38
María del Mar García <116bmargar@gmail.com>
2026-01-31 12:03:28 +0100

    changes

---
+++ controllers/PostControllers.js
@@ -0,0 +1,74 @@
+const Post = require('../models/Post')
+
+const postController ={
+
+    createPost: async (req, res) =>{
+        try{
+            const post = await Post.create(req.body)
+            res.json(post)
+        }catch (error){
+        console.error(error)
+        res.status(500).json('Error')
+        }
+    },
+
+    getAllPost: async (req, res) =>{
+        try{
+            const post = await Post.find()
+            res.json(post)
+        }catch (error){
+        console.error(error)
+        res.status(500).json('Error')
+        }
+    },
+
+    getPostById: async (req, res) =>{
+        try{
+            const post = req.params._id
+            const _id = await Post.findById(_id)
+            res.json(post)
+        }catch (error){
+        console.error(error)
+        res.status(500).json('Error')
+        }
+    }, 
+    getPostByTitle: async (req, res) =>{
+        try{
+            const title= req.params.title
+            const post = await Post.find(title)
+            res.json(post)
+        }catch (error){
+        console.error(error)
+        res.status(500).json('Error')
+        }
+    }, 
+    ChangeId: async (req, res) =>{
+        try{
+            const _id = req.params._id
+            const post = await Post.findById(_id)
+            const newId =req.params._id
+            post.id=newId
+            await post.save ()
+            res.json(post)
+        }catch (error){
+        console.error(error)
+        res.status(500).json('Error')
+        }
+    },
+    deletePost: async (req, res) =>{
+        try{
+            const _id = req.params._id
+            const post = await Post.findByIdAndDelete (_id)
+            res.json(post)
+        }catch (error){
+        console.error(error)
+        res.status(500).json('Error')
+    }
+}
+}
+
+    module.exports= postController
+
+
+
+
--- routes/post.js
+++ routes/post.js
@@ -1,79 +1,29 @@
 const express = require ('express')
 const router = express.Router()
 const Post = require ('../models/Post')
+const postController = require('../controllers/PostControllers')
 
 
-router.get ('/', (req, res)=>{
-    res.send('post')
-})
 //- POST /create: Endpoint para crear una publicación.
-router.post('/create', async (req, res) => {
-    try {
-    const post = await Post.create(req.body);
-    res.status(201).json(post);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post('/create', postController.createPost)
 
-function addPost (title, body){
-    const newPost = {
-        id: id++, 
-        title, 
-        body
-    }
-
-    if (title === undefined || body === undefined){
-        throw new Error (' Title and body are required')
-    }
-};
 
 
 //- GET /: Endpoint para traer todas las publicaciones.
-router.get('/', async (req, res) => {
-  const posts = await Post.find();
-  res.json(posts);
-});
+router.get('/', postController.getAllPost)
 
-function getPost (){
-    return posts
-};
 //- GET /id/:_id: Endpoint para buscar publicación por id.
-router.get('/id/:_id', async (req, res)=>{
-    const id =await Post.findById(id);
-    res.json (id);
-});
-
-function getPostId (postId) {
-    const posts = Post.find ((post) => post.id === postId);
-    return posts
-};
+router.get('/id/:_id', postController.getPostById)
 
 
 //- GET /title/:title: Endpoint para buscar una publicación por su titulo.
-router.get('/title/:title', async (req, res)=>{
-    const post = await Post.findOne({title: req.params.title});
-    res.json (post);
-
-});
-
-function getPostTitle (postTitle) {
-    const posts = Post.find ((post) => post.title === postTitle);
-    return posts
-};
-
+router.get('/title/:title', postController.getPostByTitle)
 
 //- PUT /id/:_id: Endpoint para actualizar una publicación.
-router.put ('/id/:_id', async (req, res)=>{
-    const id = await Post.findByIdAndUpdate(req.params._id, req.body, {new: true});
-    res.json (id);
-});
+router.put ('/id/:_id',postController.ChangeId)
 
 //- DELETE /id/:_id: Endpoint para eliminar una publicación.
-router.delete ('/id/:_id', async (req, res)=>{
-    const post = await Post.findByIdAndDelete(req.params._id);
-    res.json ({message:'Post deleted'});
-})
+router.delete ('/id/:_id', postController.deletePost)
 
-module.exports ={addPost,getPost, getPostId, getPostTitle }
+
 module.exports = router
